e6c22df768f603dae0d90db065010a6ab554db08
BhaveshKumar <bk769221@gmail.com>
2023-06-02 20:45:03 +0530

    CRUD Backend

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const express = require("express")
-const { connection } = require("./configs/config")
-const { userRouter } = require("./routes/user.route")
-const { blogRouter } = require("./routes/blog.route")
-require("dotenv").config()
-const app = express()
-app.use(express.json())
-app.use("/users",userRouter)
-app.use("/blogs",blogRouter)
+const express = require("express");
+const cors = require("cors");
+const { connection } = require("./configs/config");
+const { userRouter } = require("./routes/user.route");
+const { blogRouter } = require("./routes/blog.route");
+require("dotenv").config();
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+app.use("/users", userRouter);
+app.use("/blogs", blogRouter);
 
-app.get("/",(req,res)=>{
-res.send("this is home page")
-})
-app.listen(process.env.PORT,async()=>{
-    try {
-        await connection
-        console.log("conected to DB")
-    } catch (error) {
-    console.log(error)
-    }
-    console.log(`server running on port ${process.env.PORT}`)
-})
+app.get("/", (req, res) => {
+  res.send("this is home page");
+});
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection;
+    console.log("conected to DB");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(`server running on port ${process.env.PORT}`);
+});
